43f64f9f6eb7e1a91f50b7321151b5490526502d
alexlu <alexanderlukicev@gmail.com>
2025-03-01 20:12:26 +0200

    removed some comment

--- test_version.cpp
+++ test_version.cpp
@@ -5,7 +5,6 @@ TEST(VersionTest, ValidVersion) {
     EXPECT_GT(version(), 0);
 }
 
-// Главная функция GoogleTest
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
