89189e65f0861e2835c101f99770bdf8dfec8a84
alexlu <alexanderlukicev@gmail.com>
2025-05-10 17:59:18 +0200

    An initial code for a commands parser.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include "lib.h"
+#include "BulkParser.h"
 
 #include <iostream>
 
-int main(int, char **) {
-	std::cout << "Version: " << version() << std::endl;
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+int main(int argc, char* argv[]) 
+{
+    if (argc != 2) {
+        cerr << "An argumets' amount is wrong. Should be an argument N provided!" << endl;
+        return 1;
+
+    }
+
+    size_t N = stoi(argv[1]);
+    BulkParser parser(N);
+    string command;
+
+    while (getline(cin, command)) 
+    {
+        if (command == "") // the empty string as an exit from an input mode
+            break;
+        parser.ProcessCommand(command);
+    }
+
+    parser.Finalize();
+    return 0;
 }
